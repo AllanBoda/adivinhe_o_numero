221ee089183b6a5440b7d011578a846ec47521f4
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2023-06-20 20:31:36 -0300

    Update GreetServer.java

--- catolicasc/server/GreetServer.java
+++ catolicasc/server/GreetServer.java
@@ -6,92 +6,66 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class GreetServer {
     private ServerSocket serverSocket;
-    private List<Socket> clients;
-    private List<PrintWriter> clientWriters;
-    private List<BufferedReader> clientReaders;
-    private List<Integer> numbersToGuess;
-    private int currentPlayer;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private int numberToGuess;
 
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        clients = new ArrayList<>();
-        clientWriters = new ArrayList<>();
-        clientReaders = new ArrayList<>();
-        numbersToGuess = new ArrayList<>();
-        currentPlayer = -1;
-
         System.out.println("Servidor iniciado. Aguardando conexão...");
+        String quitMessage = "";
 
-        // Aguardar a conexão de dois jogadores
-        while (clients.size() < 2) {
-            Socket clientSocket = serverSocket.accept();
+        while (!quitMessage.equals("!quit")) {
+            clientSocket = serverSocket.accept();
             System.out.println("Conexão estabelecida com o cliente.");
-            clients.add(clientSocket);
-            clientWriters.add(new PrintWriter(clientSocket.getOutputStream(), true));
-            clientReaders.add(new BufferedReader(new InputStreamReader(clientSocket.getInputStream())));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            numberToGuess = generateRandomNumber();
+            out.println("Digite um número entre 1 e 10:");
+
+            gameHandler();
+
+            quitMessage = in.readLine();
+            System.out.println("Cliente encerrou a conexão.");
+            break;
         }
 
-        // Escolher aleatoriamente um jogador para começar
-        Random random = new Random();
-        currentPlayer = random.nextInt(2);
-
-        // Jogador 1 envia o número para o Jogador 2
-        int numberToGuess = generateRandomNumber();
-        numbersToGuess.add(numberToGuess);
-        clientWriters.get(0).println("Digite um número entre 1 e 10:");
-        clientWriters.get(1).println("Seu oponente está escolhendo um número. Aguarde...");
-
-        // Jogador 2 envia o número para o Jogador 1
-        numberToGuess = generateRandomNumber();
-        numbersToGuess.add(numberToGuess);
-        clientWriters.get(1).println("Digite um número entre 1 e 10:");
-        clientWriters.get(0).println("Seu oponente está escolhendo um número. Aguarde...");
-
-        // Iniciar o jogo
-        playGame();
-
-        // Finalizar os servidores
         stop();
     }
 
-    private void playGame() throws IOException {
-        boolean gameFinished = false;
+    private void gameHandler() throws IOException {
+        boolean correctGuess = false;
 
-        while (!gameFinished) {
-            int currentPlayerIndex = currentPlayer % 2;
-            int opponentPlayerIndex = (currentPlayer + 1) % 2;
-
-            String guess = clientReaders.get(currentPlayerIndex).readLine();
-
-            if (guess.equals("!quit")) {
-                break;
-            }
+        while (!correctGuess) {
+            String guess = in.readLine();
 
             try {
                 int number = Integer.parseInt(guess);
 
-                if (number == numbersToGuess.get(opponentPlayerIndex)) {
-                    clientWriters.get(currentPlayerIndex).println("Você acertou! Parabéns!");
-                    clientWriters.get(opponentPlayerIndex).println("Seu oponente acertou! Você perdeu.");
-                    gameFinished = true;
-                } else if (number < numbersToGuess.get(opponentPlayerIndex)) {
-                    clientWriters.get(currentPlayerIndex).println("O número é maior. Tente novamente:");
+                if (number == numberToGuess) {
+                    out.println("Você acertou! Parabéns!");
+                    correctGuess = true;
+                } else if (number < numberToGuess) {
+                    out.println("O número é maior. Tente novamente:");
                 } else {
-                    clientWriters.get(currentPlayerIndex).println("O número é menor. Tente novamente:");
+                    out.println("O número é menor. Tente novamente:");
                 }
             } catch (NumberFormatException e) {
-                clientWriters.get(currentPlayerIndex).println("Entrada inválida. Digite um número válido:");
+                out.println("Entrada inválida. Digite um número válido:");
             }
 
-            currentPlayer++;
+            if (!correctGuess) {
+                out.println("Digite um número entre 1 e 10:");
+            }
         }
     }
+
 
     private int generateRandomNumber() {
         Random random = new Random();
@@ -100,15 +74,9 @@ public class GreetServer {
 
     public void stop() {
         try {
-            for (PrintWriter writer : clientWriters) {
-                writer.close();
-            }
-            for (BufferedReader reader : clientReaders) {
-                reader.close();
-            }
-            for (Socket client : clients) {
-                client.close();
-            }
+            in.close();
+            out.close();
+            clientSocket.close();
             serverSocket.close();
         } catch (IOException ex) {
             System.out.println("Erro ao fechar a conexão.");
